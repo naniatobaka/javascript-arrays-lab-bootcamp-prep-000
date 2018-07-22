800c40270175ff1d6028f949960205bc402b8fd9
Ashley Wong <ashwong04@gmail.com>
2018-07-22 18:16:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function destructivelyAppendKitten(name){
   return kittens
 }
 
-function destrictivelyPrependKitten(name){
+function destructivelyPrependKitten(name){
   kittens.unshift(name);
   return kittens
 }
